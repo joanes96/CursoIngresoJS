d03acde9d908698e90e255ddaf7aae116af45708
joanes96 <36265043+joanes96@users.noreply.github.com>
2018-02-15 12:18:57 -0300

    ejercicios del 1 al 10 correjidos

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,33 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+		var nota
+		nota=parseInt((Math.random()*(11-1))+0)
+		alert(nota);
+		if(nota>=9)
+		{
+			alert("excelente");
+		}
+		else if (nota>=6)
+		{
+			alert("muy bien");
+		}
+		else if (nota>=4)
+		{
+			alert("bien");
+		}
+		else if (nota>=2)
+		{
+			alert("mal");
+		}
+		else if (nota>=0)
+		{
+			alert("desastrozo");
+		}
+
+
+
+
+
 
 }//FIN DE LA FUNCIÓN
