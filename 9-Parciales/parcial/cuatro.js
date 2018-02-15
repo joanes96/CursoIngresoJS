6ff85b0302a2ad1b4820d5e923b1642c2c8db8fb
joanes96 <36265043+joanes96@users.noreply.github.com>
2018-02-15 11:38:15 -0300

    parcial ejercicio 4 correjido

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,35 @@
 function Mostrar()
 {
 
+    var uno
+    var dos
+    var resultado
+    uno= prompt("ingrese numero");
+    dos= prompt("ingrese numero");
+    uno=parseInt(uno);
+    dos=parseInt(dos);
+    if(uno==dos)
+    {
+        resultado=uno*dos
+         
+    }
+    else
+    {
+        if(uno>dos)
+        {
+            resultado=uno-dos
+             
+        }
+        else
+        {
+            resultado=uno+dos
+                        
+        }
+    }
+
+    document.write(resultado);
+
+
+
+
 }
