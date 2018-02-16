8d90414a3c63a97c502a26367e7766ce113dc08c
joanes96 <36265043+joanes96@users.noreply.github.com>
2018-02-16 12:05:57 -0300

    se aprendio a usar el swtich, case y break.

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,7 +4,25 @@ function Mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+    switch(mesDelAño)
+    {
+        case "Enero" :
+        case "Febrero" :
+        case "Marzo" :
+        case "Abril" :
+            alert("Si tiene 31 dias")
+            break;
+        case "Mayo" :
+        case "Junio" :
+        case "Julio" :
+        case "Agosto" :
+        case "Septiembre" :
+            alert("Si tiene 30 dias")
+            break;
+        default :
+            alert("Si tiene 28 dias")
+            break;
+    }
 	
 
 
