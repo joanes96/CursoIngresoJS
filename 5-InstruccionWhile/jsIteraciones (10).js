dd48954bd4c9fda88dc5d72e60d505f0b9cf9e39
joanes96 <36265043+joanes96@users.noreply.github.com>
2018-02-23 12:08:37 -0300

    ejer de 1 a 10

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -5,12 +5,58 @@ function Mostrar()
 	//declarar contadores y variables 
 	
 	var respuesta="si";
-
+	var positivos=0;
+	var negativos=0;
+	var contadorPositivos=0;
+	var contadorNegativos=0;
+	var contadorCeros=0;
+	var contadorPares=0;
+	var numero;
+	var promedioPositivos;
+	var promedioNegativos;
+	var diferencia;
 	while(respuesta!="no")
 	{
+		contador++;
+		numero=prompt("Ingrese Numero");
+		numero=parseInt(numero);
+		while(isNaN(numero))
+		{
+			numero=prompt();
+			numero=parseInt(numero);
+		}
+		if(numero%2==0 && numero!=0)
+		{
+			contadorPares++;
+		}
+		if(numero<0)
+		{
+			negativos=negativos+numero;
+			contadorNegativos++;
+		}
+		else
+		{
+			if (numero>0)
+			{
+				positivos=positivos+numero;
+				contadorPositivos++;
+			}
+			else
+			{
+				contadorCeros++;
+			}
+		}
 		
+
+	respuesta=prompt("Desea continuar?")
 	
 	}
+	promedioNegativos=negativos/contadorNegativos;
+	promedioPositivos=positivos/contadorPositivos;
+	diferencia=positivos-negativos
+	document.write("<br>"+);
+	
+	
 
 
 
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,15 +3,39 @@ function Mostrar()
 
 	var contador=0;
 	// declarar variables
-	
+	var numeroMax;
+	var numeroMin;
+	var numero
 	var respuesta='si';
 
 	while(respuesta!='no')
 	{
 		
+		numero=prompt("Ingrese Numero");
+		numero=parseInt(numero);
+		if (contador==0)
+		{
+			numeroMax=numero;
+			numeroMin=numero;
+		}
+		else
+		{
+			if(numero>numeroMax)
+			{
+			numeroMax=numero;
+			}
+			if (numero<numeroMin)
+			{
+			numeroMin=numero;
+			}
+		}
+		contador++;
+
+		respuesta=prompt("Quiere continuar?")
 	
 	}
-
+document.getElementById("maximo").value=numeroMax;
+document.getElementById("minimo").value=numeroMin;
 
 
 
