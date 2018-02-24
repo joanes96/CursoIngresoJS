ca5b2fbfeb16dba38d5fb540460e53d14fa31e84
joanes96 <36265043+joanes96@users.noreply.github.com>
2018-02-24 14:48:03 -0300

    while comprendido!

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -2,12 +2,14 @@ function Mostrar()
 {
 
 var clave
+var intentos=0;
 clave=prompt("Ingrese clave")
-while(clave!="utn750" && clave!=null)
+while(clave!="utn750" && clave!=null &&intentos<2)
 {
 
 	clave=prompt("Clave Erronea")
+	intentos++;
 } 
-
-
+if(intentos!=2)
+alert("Bienvenidos al Sistema")
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -2,10 +2,12 @@ function Mostrar()
 {
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
-	while(numero<0 || numero>9)
+	numero= parseInt(numero);
+	while(isNaN(numero) || numero<0 || numero>10)
 	{
-		numero=prompt("Numero erroneo.")
+		numero=prompt("Numero erroneo.");
+		numero= parseInt(numero);
 	}
-
+alert("Bienvenidos");
 
 }//FIN DE LA FUNCIÓN
