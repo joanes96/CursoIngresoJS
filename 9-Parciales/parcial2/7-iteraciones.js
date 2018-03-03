b16f8b593d8b94b9ad185a55230ad8f4e20cd232
joanes96 <36265043+joanes96@users.noreply.github.com>
2018-03-03 13:11:25 -0300

    asfas

--- 9-Parciales/parcial2/7-iteraciones.js
+++ 9-Parciales/parcial2/7-iteraciones.js
@@ -1,7 +1,43 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 7-iteraciones");
-	
+	var numero;
+	var seguir;
+	var cantidadDePares=0;
+	var acumulador=0;
+	var promedio;
+	var minimo
+	var maximo
+
+	while(seguir==true)
+	{
+		numero=prompt("Ingrese Numero");
+		numero=parseInt(numero);
+		while(numero<=0 || isNaN(numero))
+		{
+			numero=prompt("Ingrese Numero");
+			numero=parseInt(numero);
+		}
+		if(numero%2==0)
+		{
+			cantidadDePares++;
+
+		}
+
+		acumulador=acumulador+numero;
+
+		if(contador==0 || numero<minimo)
+		{
+			minimo=numero;
+		}
+		if(contador==0 || numero<maximo)
+		{
+			maximo=numero;
+		}
+
+		contador++;
+		seguir=confirm("desea ingresar otros?");
+	}
+	promedio=acumulador/contador;
 }
 
